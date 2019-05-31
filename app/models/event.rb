288a8d450c47d49a8bafafcd435018123714c9ca
Josefina Valenzuela <jrvalenzuela@miuandes.cl>
2019-05-31 16:10:06 -0400

    Second commit for lab11

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
   accepts_nested_attributes_for :ticket_types
+  has_attached_file :avatar, style: { medium: "140x140>", thumb: "10x10>"}, default_url: "/images/:style/missing.png", validate_media_type: false
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
