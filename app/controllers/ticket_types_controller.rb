04f058fbf135f2d5523da10436395a08cc579295
Josefina Valenzuela <jrvalenzuela@miuandes.cl>
2019-05-31 22:58:31 -0400

    Fifth commit for lab11

--- app/controllers/ticket_types_controller.rb
+++ app/controllers/ticket_types_controller.rb
@@ -1,13 +1,55 @@
 class TicketTypesController < ApplicationController
 
 
+  def index
+    @ticket_types = TicketType.all
+  end
+ 
+  def show
+    @ticket_type = TicketType.find(params[:id])
+  end
+ 
+  def new
+    @ticket_type = TicketType.new
+  end
+ 
+  def edit
+    @ticket_type = TicketType.find(params[:id])
+  end
+ 
+  def create
 
+    @ticket_type = TicketType.new(ticket_type_params)
+ 
+    if @ticket_type.save(ticket_type_params)
 
-	def new
-    	@ticket_type = TicketType.new
-    	2.times { @ticket_type.ticket_zones.build }
- 	end
-	def event_params
-      params.fetch(:ticket_type, {}).permit(:ticket_zone_id, :price, :event_id, ticket_zone_attributes: [:zone])
+      redirect_to @ticket_type
+    else
+      render 'new'
+    end
+
+  end
+ 
+  def update
+    @ticket_type = TicketType.find(params[:id])
+ 
+    if @ticket_type.update(ticket_type_params)
+      redirect_to @ticket_type
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @ticket_type = TicketType.find(params[:id])
+    @ticket_type.destroy
+ 
+    redirect_to articles_path
+  end
+ 
+  private
+	
+	def ticket_type_params
+      params.require(:ticket_type).permit(:ticket_zone_id, :price, :event_id)
     end
 end
